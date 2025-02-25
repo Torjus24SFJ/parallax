0d361044c0a15590ab2946bd0ad1163cd6a31b45
Torjus <Torjus24sfj@gmail.com>
2025-02-25 14:54:49 +0100

    layers

--- src/App.jsx
+++ src/App.jsx
@@ -3,9 +3,12 @@ import { Parallax } from "./components/Parallax";
 function App() {
   return (
     <>
-    <Parallax />
+      <main className="font-josefin font-bold">
+        <Parallax />
+        <div className="w-full bg-[#06141D]"></div>
+      </main>
     </>
-  )
+  );
 }
 
 export default App;
--- src/components/Parallax.jsx
+++ src/components/Parallax.jsx
@@ -1,9 +1,6 @@
 export function Parallax() {
-  return (
-    <>
-      <div className="flex flex-col place-items-center mt-10">
-        <h1 className="font-josefin font-bold text-7xl">Parallax</h1>
-      </div>
-    </>
-  );
+    return (
+        <>
+        </>
+    )
 }
